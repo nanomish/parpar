397ad779169f229529bccc21308ad890750a9fb3
nanomish <michael_kushniriov@edmont.c.parkings-helper.internal>
2016-07-03 18:10:55 +0000

    working taking pictures

---
+++ public/app/main/main.controller.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('parparApp')
+  .controller('MainCtrl', function ($scope, $http, socket) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+      socket.syncUpdates('thing', $scope.awesomeThings);
+    });
+
+    $scope.addThing = function() {
+      if($scope.newThing === '') {
+        return;
+      }
+      $http.post('/api/things', { name: $scope.newThing });
+      $scope.newThing = '';
+    };
+
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('thing');
+    });
+
+	$scope.onSuccess = function(data) {
+        console.log(data);
+    };
+    $scope.onError = function(error) {
+        console.log(error);
+    };
+    $scope.onVideoError = function(error) {
+        console.log(error);
+    };   
+  });
